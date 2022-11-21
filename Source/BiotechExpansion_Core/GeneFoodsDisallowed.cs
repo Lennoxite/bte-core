83425853ab616f35994c8d8e15b6d88071ad6c2f
Lennoxicon <pinoy1999@gmail.com>
2022-11-21 11:35:34 -0700

    crossexpan update

---
+++ Source/BiotechExpansion_Core/GeneFoodsDisallowed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+namespace BTE
+{
+    public class GeneFoodsDisallowed : DefModExtension
+    {
+        public FoodKind foodKind;
+    }
+}
