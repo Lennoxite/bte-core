def658df7d2de8770ad297f8437fa1f901c7f143
Lennoxicon <pinoy1999@gmail.com>
2024-03-13 19:51:12 -0700

    1.5 modding

---
+++ 1.5/Source/BiotechExpansion_Core/BTEMMLA_ThingDefOf.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BTE
+{
+	[DefOf]
+	public static class BTE_ThingDefOf
+	{
+		static BTE_ThingDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(BTE_ThingDefOf));
+		}
+
+		public static ThingDef Milk;
+		public static ThingDef WoolSheep;
+
+
+
+	}
+
+}
