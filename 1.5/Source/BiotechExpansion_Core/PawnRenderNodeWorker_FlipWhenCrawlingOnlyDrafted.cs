69040ae12f7ed8a2f2248d1b761d6477de5ca494
Lennoxicon <pinoy1999@gmail.com>
2024-03-19 20:08:23 -0700

    Genes that render only in combat/draft

---
+++ 1.5/Source/BiotechExpansion_Core/PawnRenderNodeWorker_FlipWhenCrawlingOnlyDrafted.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BTE
+{
+    class PawnRenderNodeWorker_FlipWhenCrawlingOnlyDrafted : PawnRenderNodeWorker_FlipWhenCrawling
+    {
+
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+
+            if (PawnUtility.IsAttacking(parms.pawn) || parms.pawn.Drafted)
+            {
+                return base.CanDrawNow(node, parms);
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
