59b413e7f6716d9b694e84967757b77483956115
Lennoxicon <pinoy1999@gmail.com>
2024-03-22 14:36:21 -0700

    new floating sprites

---
+++ 1.5/Source/BiotechExpansion_Core/PawnRenderNodeWorker_OnlyDraftedPulsing.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using UnityEngine;
+namespace BTE
+{
+    class PawnRenderNodeWorker_OnlyDraftedPulsing : PawnRenderNodeWorker_OnlyDrafted
+    {
+
+        public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
+        {
+			Vector3 floater = base.OffsetFor(node, parms, out pivot);
+			floater.z += 0.05f + 0.05f * (float)Math.Sin(Find.TickManager.TicksGame/30);
+            return floater;
+			
+        }
+        /*protected override Material GetMaterial(PawnRenderNode node, PawnDrawParms parms)
+		{
+			if (alternatingNode == null)
+			{
+				if (!cacheRenderNode(node))
+				{
+					return null;
+				}
+			}
+			currentGraphic = alternatingNode.Graphics;
+			if (currentGraphic == null)
+			{
+				return null;
+			}
+			if (node.Props.flipGraphic && parms.facing.IsHorizontal)
+			{
+				parms.facing = parms.facing.Opposite;
+			}
+			Material material = currentGraphic.NodeGetMat(parms);
+			if (!parms.Portrait && parms.flags.FlagSet(PawnRenderFlags.Invisible))
+			{
+				material = InvisibilityMatPool.GetInvisibleMat(material);
+			}
+			return material;
+		}
+
+        public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
+		{
+			ticks--;
+			if (ticks <= 0)
+            {
+				ticks = tickInterval;
+
+				if (alternatingNode == null)
+				{
+					if (!cacheRenderNode(node))
+					{
+						return base.OffsetFor(node, parms, out pivot);
+					}
+				}
+
+				alternatingNode.alternateGraphic();
+			}
+
+
+
+			return base.OffsetFor(node, parms, out pivot);
+        }
+
+        private bool cacheRenderNode(PawnRenderNode node)
+        {
+			if ((alternatingNode = (node as PawnRenderNode_AttachmentHeadAlternating)) != null)
+            {
+				return true;
+            }
+			return false;
+        }
+		private Graphic currentGraphic = null;
+		private PawnRenderNode_AttachmentHeadAlternating alternatingNode = null;
+
+
+		private int ticks = 0;
+		private int tickInterval = 180;*/
+    }
+
+}
---
+++ 1.5/Source/BiotechExpansion_Core/PawnRenderNode_AttachmentHeadAlternating.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace BTE
+{
+    class PawnRenderNode_AttachmentHeadAlternating : PawnRenderNode_AttachmentHead
+    {
+        public PawnRenderNode_AttachmentHeadAlternating(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree) {
+
+        }
+
+        public Graphic Graphics
+        {
+            get {
+                return graphics[currentIndex]; 
+            }
+        }
+
+        protected override void EnsureMaterialsInitialized()
+        {
+            if (this.graphics.Empty() && this.HasGraphic(this.tree.pawn))
+            {
+                this.GraphicsFor(this.tree.pawn);
+                texSize = this.props.texPaths.Count;
+                graphic = graphics[0];
+                
+            }
+        }
+
+        public virtual List<Graphic> GraphicsFor(Pawn pawn)
+        {
+
+            
+            if (this.props.texPaths.NullOrEmpty())
+            {
+                return null;
+            }
+
+            Shader shader = this.ShaderFor(pawn);
+            if (shader == null)
+            {
+                return null;
+            }
+
+            foreach (string str in this.props.texPaths)
+            {
+                graphics.Add(GraphicDatabase.Get<Graphic_Multi>(str, shader, Vector2.one, this.ColorFor(pawn)));
+            }
+            return graphics;
+        }
+
+        public void alternateGraphic()
+        {
+            Log.Message("New graphic for! " + this.tree.pawn.Name.ToString());
+            currentIndex = Rand.Range(0, texSize);
+            requestRecache = true;
+
+        }
+
+        //public override Graphic GraphicFor(Pawn pawn)
+        //{
+        //    return base.GraphicFor(pawn);
+        //}
+
+        public List<Graphic> graphics = new List<Graphic>();
+        public int currentIndex = 0;
+
+        private int ticks = 0;
+        private int tickInterval = 180;
+        private int texSize = 0;
+    }
+
+}
+
