d337b1e8ee8254be310a5134681934084a4e0a49
Lennoxicon <pinoy1999@gmail.com>
2024-03-31 20:39:29 -0700

    Variable Rotator render class

---
+++ 1.5/Source/BiotechExpansion_Core/PawnRenderNodeProperties_VariableRotator.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BTE
+{
+    public class PawnRenderNodeProperties_VariableRotator : PawnRenderNodeProperties
+    {
+        public PawnRenderNodeProperties_VariableRotator()
+        {
+            workerClass = typeof(PawnRenderNodeWorker_Eye);
+            nodeClass = typeof(PawnRenderNode_AttachmentHead);
+        }
+
+        public float degreesPerTick = 1f;
+    }
+}
